02da7552a9a56e90e3377949278d52dbc5e23a0b
Elenadelatorre <146189689+Elenadelatorre@users.noreply.github.com>
2026-06-19 16:09:24 +0200

    Enhance XML parsing security in XmlController
    Updated XML parsing method to mitigate XXE attacks by disabling DTDs and external entity references.

--- src/java/src/main/java/com/example/api/controller/XmlController.java
+++ src/java/src/main/java/com/example/api/controller/XmlController.java
@@ -9,33 +9,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+
 @RestController
 @RequestMapping("/api/xml")
 public class XmlController {
 
-    // VULNERABLE (punto de inicio del ejercicio):
-    // @PostMapping("/parse")
-    // public ResponseEntity<?> parseXml(@RequestBody String xmlInput) throws Exception {
-    //     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    //     DocumentBuilder builder = factory.newDocumentBuilder();
-    //     Document doc = builder.parse(new ByteArrayInputStream(xmlInput.getBytes()));
-    //     return ResponseEntity.ok(doc.getDocumentElement().getTagName());
-    // }
-    //
-    // Un atacante puede enviar:
-    // <!DOCTYPE foo [<!ENTITY xxe SYSTEM "file:///etc/passwd">]>
-    // <data>&xxe;</data>
-    // El servidor leera /etc/passwd y lo incluira en la respuesta.
-
     @PostMapping("/parse")
     public ResponseEntity<?> parseXml(@RequestBody String xmlInput) throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        
+        // Mitigación contra ataques XXE
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        factory.setXIncludeAware(false);
+        factory.setExpandEntityReferences(false);
+
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new ByteArrayInputStream(xmlInput.getBytes()));
+        Document doc = builder.parse(new InputSource(new StringReader(xmlInput)));
+        
         return ResponseEntity.ok(doc.getDocumentElement().getTagName());
     }
 }
+
